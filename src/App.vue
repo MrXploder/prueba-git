9b95a1a40637ffa863c4297f5031e65dce78f265
Luis Arancibia <l.arancibiaf@gmail.com>
2022-04-27 21:55:36 -0400

    mi primer commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col mx-auto text-center">
+        <TodoList nombre="deberes"/>
+        <TodoList nombre="supermercado"/>
+        <TodoList />
+        <TodoList />
+        <TodoList />
+        <TodoList />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoList from "@/components/TodoList.vue";
+export default {
+  name: "App",
+  components: { TodoList },
+};
+</script>
+
+<style></style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="card p-4">
+    <h4 class="text-secondary mt-4">Lista {{ nombre }}</h4>
+    <form class="mt-4" @submit.prevent="agregarNuevaTarea()">
+      <div class="form-group row">
+        <div class="col-9">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Ingresa una nueva tarea"
+            required
+            v-model="nuevaTarea"
+          />
+        </div>
+        <div class="col-2">
+          <button type="submit" class="btn btn-outline-secondary">
+            Agregar
+          </button>
+        </div>
+      </div>
+    </form>
+    <h4 class="text-secondary mt-4">Lista</h4>
+    <ul class="list-group">
+      <li
+        class="list-group-item"
+        v-for="(tarea, $index) of tareas"
+        :key="$index"
+      >
+        {{ $index + 1 }}: {{ tarea }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  props: {
+    nombre: { type: String, default: "X" },
+  },
+  data: () => ({
+    nuevaTarea: "",
+    tareas: [],
+  }),
+  methods: {
+    agregarNuevaTarea() {
+      this.tareas.unshift(this.nuevaTarea);
+      this.nuevaTarea = "";
+    },
+  },
+};
+</script>
+
+<style></style>
